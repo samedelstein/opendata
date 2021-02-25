433ce0cdd27f423a31ce37a4bc19add31f479a66
Sam Edelstein <sam.i.edelstein@gmail.com>
2021-02-25 16:20:41 +0000

    updated city table and added packages and macros to enable primary key formation

---
+++ models/city.sql
@@ -0,0 +1,39 @@
+with nyc311 as (
+
+    select city, state  from {{ ref('stg_nyc311') }}
+
+),
+
+chicago311 as (
+
+    select city, state  from {{ ref('stg_chicago311') }}
+
+),
+
+boston311 as (
+
+    select city, state  from {{ ref('stg_boston311') }}
+
+),
+
+ citystate as (
+select * from nyc311
+
+UNION DISTINCT 
+
+select * from chicago311
+
+UNION DISTINCT 
+
+select * from boston311
+)
+
+select   {{ dbt_utils.surrogate_key(
+      'city',
+      'state'
+  ) }} as CityKey,
+  city,
+  state
+  from citystate
+
+
