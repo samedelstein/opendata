552466c2015b6af006bc895f9d78ff68c0343747
Sam Edelstein <sam.i.edelstein@gmail.com>
2021-02-26 17:02:17 +0000

    added kc and updated timetocomplete

--- models/timetocomplete.sql
+++ models/timetocomplete.sql
@@ -6,8 +6,8 @@ with stg311 as (
   , complainttypekey
   , agencykey
   , statuskey
-  , DATE_DIFF(DATE (closeddate)
-  ,DATE( createddate),  DAY) as DateDiff 
+  , CASE WHEN ClosedDate IS NOT NULL THEN DATE_DIFF(DATE (closeddate) ,DATE( createddate),  DAY) ELSE NULL END as DaysToClose
+  , CASE WHEN ClosedDate IS NULL THEN DATE_DIFF(Current_Date, DATE( createddate),  DAY) ELSE NULL END as DaysOpened
 FROM {{ ref('stg_311') }} stg 
 join {{ ref('city') }} city 
   on stg.city = city.city and stg.state = city.state
