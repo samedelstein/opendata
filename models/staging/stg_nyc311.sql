7eb8c4385c730140259a2d0e304d94a56a1ef08b
Sam Edelstein <sam.i.edelstein@gmail.com>
2021-03-23 11:07:00 +0000

    updated ref

--- models/staging/stg_nyc311.sql
+++ models/staging/stg_nyc311.sql
@@ -1,12 +1,12 @@
-SELECT * FROM {{ ref('stg_nyc311') }} where UniqueKey not in (select UniqueKey from {{ ref('base_nyc311') }})
+SELECT * FROM opendatadbt.dbt_sedelstein.stg_nyc311 where UniqueKey not in (select UniqueKey from opendatadbt.dbt_sedelstein.base_nyc311)
 
 union all
 
-select {{ dbt_utils.current_timestamp() }} as RowCreatedDateTime, {{ dbt_utils.current_timestamp() }} AS RowUpdatedDateTime, * from {{ ref('base_nyc311') }}  where UniqueKey not in (select UniqueKey from {{ ref('stg_nyc311') }} )
+select {{ dbt_utils.current_timestamp() }} as RowCreatedDateTime, {{ dbt_utils.current_timestamp() }} AS RowUpdatedDateTime, * from opendatadbt.dbt_sedelstein.base_nyc311  where UniqueKey not in (select UniqueKey from opendatadbt.dbt_sedelstein.stg_nyc311 )
 
 UNION ALL
 
-select stg.RowCreatedDateTime, {{ dbt_utils.current_timestamp() }}, base.* from {{ ref('stg_nyc311') }}  stg join {{ ref('base_nyc311') }}  base on stg.UniqueKey = base.UniqueKey
+select stg.RowCreatedDateTime, {{ dbt_utils.current_timestamp() }}, base.* from opendatadbt.dbt_sedelstein.stg_nyc311  stg join opendatadbt.dbt_sedelstein.base_nyc311 base on stg.UniqueKey = base.UniqueKey
 
 
 
