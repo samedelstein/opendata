9e211eab682e03d2b9070900655c9e60692b72f0
Sam Edelstein <sam.i.edelstein@gmail.com>
2021-03-30 00:14:47 +0000

    added census data

---
+++ models/fct_censusdata.sql
@@ -0,0 +1,13 @@
+with points as (
+    select * from {{ref('timetocomplete')}} 
+),
+
+tracts as (
+    select tract_geom, geo_id from {{ref('censustract')}} 
+
+)
+
+
+    SELECT count(t.geo_id) as Count_Tract, t.geo_id, p.complainttypekey, p.createddatekey, p.statuskey  FROM tracts t, points p 
+    WHERE st_contains(t.tract_geom, p.coordinates) 
+    group by p.complainttypekey, p.createddatekey, p.statuskey, t.geo_id
